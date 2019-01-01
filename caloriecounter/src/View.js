b72502b1fed78e57f34114fdf819504d71e8577d
bmihovski <bmihovski@live.com>
2019-01-01 17:10:29 +0200

    Add record frontend only implemented

--- caloriecounter/src/View.js
+++ caloriecounter/src/View.js
@@ -1,16 +1,20 @@
 import hh from 'hyperscript-helpers';
 import { h } from 'virtual-dom';
-import { showFormMsg } from './Update.js';
+import { showFormMsg,
+         mealInputMsg,
+         caloriesInputMsg
+       } from './Update.js';
 
 const { pre, div, h1, button, form, label, input } = hh(h);
 
-function fieldSet(labelText, inputValue) {
+function fieldSet(labelText, inputValue, oninput) {
   return div([
         label({ className: 'db mb1' }, labelText),
          input({
            className: 'pa2 input-reset ba w-100 mb2',
            type: 'text',
-           value: inputValue
+           value: inputValue,
+           oninput
          }),
        ]);
      };
@@ -43,8 +47,12 @@ function formView(dispatch, model) {
         className: 'w-100 mv2',
       },
       [
-          fieldSet('Meal', description),
-          fieldSet('Calories', calories || ''),
+          fieldSet('Meal', description,
+          e => dispatch(mealInputMsg(e.target.value))
+        ),
+          fieldSet('Calories', calories || '',
+          e => dispatch(caloriesInputMsg(e.target.value))
+        ),
           buttonSet(dispatch),
       ],
     );
