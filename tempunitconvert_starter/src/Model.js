c0829928e93b2cce87443733f275471c8a2cfe08
bmihovski <bmihovski@live.com>
2019-01-02 14:30:52 +0200

    model defined

--- tempunitconvert_starter/src/Model.js
+++ tempunitconvert_starter/src/Model.js
@@ -1,5 +1,9 @@
 const initModel = {
-  
+  leftUnitToConvert: 0,
+  rightUnitToConvert: 32,
+  leftTemperatureType: 'Celsius',
+  rightTemperatureType: 'Fahrenheit',
+  sourceLeft: true,
 };
 
 export default initModel;
